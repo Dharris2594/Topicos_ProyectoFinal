e9c0a1579200e28e0f2e217cd61b2594f2bfa5d7
Dharris2594 <dharris@hypernovalabs.com>
2021-07-18 11:29:02 -0500

    Agregar archivos de proyecto.

---
+++ Topicos_ProyectoFinal/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Topicos_ProyectoFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.axWindowsMediaPlayer2 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 68);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(333, 163);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 247);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(157, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Seleccione Voz";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Traducir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 289);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(333, 46);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(455, 247);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(157, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.Text = "Seleccione Voz";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(455, 68);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(333, 163);
+            this.textBox2.TabIndex = 5;
+            // 
+            // axWindowsMediaPlayer2
+            // 
+            this.axWindowsMediaPlayer2.Enabled = true;
+            this.axWindowsMediaPlayer2.Location = new System.Drawing.Point(455, 289);
+            this.axWindowsMediaPlayer2.Name = "axWindowsMediaPlayer2";
+            this.axWindowsMediaPlayer2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer2.OcxState")));
+            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(333, 46);
+            this.axWindowsMediaPlayer2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(259, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Reproducir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(702, 247);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Reproducir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(455, 26);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(333, 21);
+            this.comboBox4.TabIndex = 10;
+            this.comboBox4.Text = "Seleccione Idioma";
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Location = new System.Drawing.Point(13, 26);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(332, 21);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "Español";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 357);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.axWindowsMediaPlayer2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Traductor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Topicos_ProyectoFinal/Form1.cs
@@ -0,0 +1,167 @@
+﻿using IBM.Cloud.SDK.Core.Authentication.Iam;
+using IBM.Watson.LanguageTranslator.v3;
+using IBM.Watson.LanguageTranslator.v3.Model;
+using IBM.Watson.TextToSpeech.v1;
+using IBM.Watson.TextToSpeech.v1.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topicos_ProyectoFinal
+{
+    public partial class Form1 : Form
+    {
+        TextToSpeechService textToSpeechService;
+        LanguageTranslatorService languageTranslatorService;
+        List<Voice> listVoices;
+        string textToSpeechKey = "g7w6YxO0ZTH-8-EE0Iwrj7ORk1eKXRim4n_b8iKKqU4-";
+        string textToSpeechUrl = "https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/89d5a008-47c4-4025-8b5d-4ca26d532bd1";
+        string languageTranslatorKey = "cWxrMKlB9HcMaxrTdSSN-x60rc8_zXBMHsMcdEzH-V-D";
+        string languageTranslatorUrl = "https://api.us-south.language-translator.watson.cloud.ibm.com/instances/80e7539f-db52-46fd-95c4-1b3e3f5c27fc";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string modelId = "es-" + comboBox4.Text.Split('(', ')')[1];
+
+                var result = languageTranslatorService.Translate(
+                text: new List<string>() { textBox1.Text },
+                modelId: modelId
+                );
+                dynamic data = JsonConvert.DeserializeObject<ExpandoObject>(result.Response);
+                textBox2.Text = data.translations[0].translation;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ingrese Texto Y Seleccione Idioma");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IamAuthenticator textToSpeechAuth = new IamAuthenticator(apikey: textToSpeechKey);
+            textToSpeechService = new TextToSpeechService(textToSpeechAuth);
+            textToSpeechService.SetServiceUrl(textToSpeechUrl);
+
+            IamAuthenticator languageTranslatorAuth = new IamAuthenticator(apikey:languageTranslatorKey);
+
+            languageTranslatorService = new LanguageTranslatorService("2018-05-01", languageTranslatorAuth);
+            languageTranslatorService.SetServiceUrl(languageTranslatorUrl);
+
+            var result = textToSpeechService.ListVoices();
+
+            listVoices = result.Result._Voices;
+
+            foreach (var voice in listVoices.Where(voice => voice.Language.StartsWith("es")))
+            {
+                comboBox1.Items.Add(voice.Name);
+            }
+
+            comboBox4.Items.Add("Inglés (en)");
+            comboBox4.Items.Add("Francés (fr)");
+        }
+
+
+        private void playAudio(string text, string voice, string errorMessage, int mediaPlayer)
+        {
+            try
+            {
+                if(mediaPlayer == 1)
+                    axWindowsMediaPlayer1.URL = null;
+                else
+                    axWindowsMediaPlayer2.URL = null;
+
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(textToSpeechUrl + "/v1/synthesize?voice=" + voice);
+                httpWebRequest.Method = "POST";
+                httpWebRequest.ContentType = "application/json";
+
+                var username = "apikey";
+                var password = textToSpeechKey;
+                string encoded = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1")
+                                               .GetBytes(username + ":" + password));
+
+                httpWebRequest.Headers.Add("Authorization", "Basic " + encoded);
+                httpWebRequest.Accept = "audio/wav";
+
+                object data = new
+                {
+                    text = text
+                };
+
+                var jsonString = JsonConvert.SerializeObject(data);
+
+                byte[] postBytes = Encoding.UTF8.GetBytes(jsonString);
+
+                httpWebRequest.ContentLength = postBytes.Length;
+
+                Stream requestStream = httpWebRequest.GetRequestStream();
+                requestStream.Write(postBytes, 0, postBytes.Length);
+                requestStream.Close();
+
+                HttpWebResponse response = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                Stream resStream = response.GetResponseStream();
+
+                string path = Path.GetTempFileName() + ".wav";
+
+                using (FileStream fs = File.Create(path))
+                {
+                    resStream.CopyTo(fs);
+                    fs.Close();
+                    resStream.Close();
+                }
+
+                if (mediaPlayer == 1)
+                    axWindowsMediaPlayer1.URL = path;
+                else
+                    axWindowsMediaPlayer2.URL = path;
+
+                File.Delete(path);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(errorMessage);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            playAudio(textBox1.Text, comboBox1.Text, "Ingrese un Texto y Seleccione Voz", 1);
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            comboBox2.Text = "Seleccione Voz";
+            textBox2.Text = "";
+            string target = comboBox4.Text.Split('(', ')')[1];
+
+            foreach (var voice in listVoices.Where(voice => voice.Language.StartsWith(target)))
+            {
+                comboBox2.Items.Add(voice.Name);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            playAudio(textBox2.Text, comboBox2.Text, "Traduzca el Texto y Seleccione Voz", 2);
+        }
+    }
+}
